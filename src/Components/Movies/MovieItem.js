328d1934b9a10a835134165b8d7bbad6d4c9d757
Narasimman2067 <narasimmamech2067@gmail.com>
2023-03-19 13:17:09 +0530

    hello

--- src/Components/Movies/MovieItem.js
+++ src/Components/Movies/MovieItem.js
@@ -12,15 +12,15 @@ export const MovieItem = ({ title, releaseDate, posterUrl, id }) => {
   return (
     <Card
       sx={{
-        width: 250,
+        width: 200,
         height: 320,
         borderRadius: 5,
-        margin: 2,
+        margin: 4,
 
         ":hover": { boxShadow: "10px 10px 20px green" },
       }}
     >
-      <img height={"50%"} width="100%" src={posterUrl} alt={title} />
+      <img height={"50%"} width="100%"  src={posterUrl} alt={title} />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {title}
@@ -36,6 +36,8 @@ export const MovieItem = ({ title, releaseDate, posterUrl, id }) => {
           color="inherit"
           sx={{ margin: "auto", color: "blue" }}
           size="small"
+          variant="contained"
+          indicatorcolor="secondary"
         >
           Book now
         </Button>
